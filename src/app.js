f3d87191e224c96331fcc9db4ef6e326307629b3
liruiqing7 <liruiqing7@163.com>
2024-01-05 11:28:36 +0800

    初始化 webpack

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        my-app-test
+        <span>👴笑了</span>
+      </div>
+    );
+  }
+}
+
+export default App;
